89c3cbcc73a1139502b07646a3ccaa8cf380ee38
Syed Mohsin Raza <mohsin.raza@express.com.pk>
2019-08-09 17:38:31 +0500

    OP-329 (Sign up - Multilingual)

--- app/src/main/java/structure/com/foodportal/fragment/foodportal/FoodSignUpFragment.java
+++ app/src/main/java/structure/com/foodportal/fragment/foodportal/FoodSignUpFragment.java
@@ -3,6 +3,7 @@ package structure.com.foodportal.fragment.foodportal;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +22,9 @@ import structure.com.foodportal.helper.UIHelper;
 public class FoodSignUpFragment extends BaseFragment implements View.OnClickListener {
 
     FragmentFoodSignupBinding binding;
+    private int mLang;
+    private String mFnameRequired, mLnameRequired, mEmailRequired, mInvalidEmail, mPhoneNumberRequired,
+            mPasswordRequired, mPasswordCharacters, mConfirmPassRequired, mPassDoesNotMatch;
 
     public FoodSignUpFragment() {
 
@@ -43,11 +47,88 @@ public class FoodSignUpFragment extends BaseFragment implements View.OnClickList
 
 
     @Override
+    public void onStart() {
+        super.onStart();
+        mLang = preferenceHelper.getSelectedLanguageIndex();
+        setValuesByLanguage(mLang);
+    }
+
+    private void setValuesByLanguage(int language) {
+        binding.linearLayoutName.removeAllViews();
+
+        switch (language) {
+            case AppConstant.Language.ENGLISH:
+                binding.tvCreateYourAccount.setText(registrationActivity.getString(R.string.create_your_account_en));
+                binding.tvFname.setText(registrationActivity.getString(R.string.fname_en));
+                binding.tvLname.setText(registrationActivity.getString(R.string.lname_en));
+                binding.tvEmail.setText(registrationActivity.getString(R.string.email_en));
+                binding.tvPhoneNumber.setText(registrationActivity.getString(R.string.phone_number_en));
+                binding.tvPassword.setText(registrationActivity.getString(R.string.password_en));
+                binding.tvConfirmPassword.setText(registrationActivity.getString(R.string.confirm_password_en));
+                binding.btnCreateAccount.setText(registrationActivity.getString(R.string.create_account_en));
+
+                binding.linearLayoutName.addView(binding.linearLayoutFirstName, 0);
+                binding.linearLayoutName.addView(binding.viewSpace, 1);
+                binding.linearLayoutName.addView(binding.linearLayoutLastName, 2);
+
+                binding.etFname.setGravity(Gravity.START);
+                binding.etLname.setGravity(Gravity.START);
+                binding.etEmail.setGravity(Gravity.START);
+                binding.etPhoneNumber.setGravity(Gravity.START);
+                binding.etPassword.setGravity(Gravity.START);
+                binding.etConfirmPassword.setGravity(Gravity.START);
+
+                mFnameRequired = registrationActivity.getResources().getString(R.string.fname_required_en);
+                mLnameRequired = registrationActivity.getResources().getString(R.string.lname_required_en);
+                mEmailRequired = registrationActivity.getResources().getString(R.string.email_required_en);
+                mInvalidEmail = registrationActivity.getResources().getString(R.string.invalid_email_en);
+                mPhoneNumberRequired = registrationActivity.getResources().getString(R.string.phone_number_required_en);
+                mPasswordRequired = registrationActivity.getResources().getString(R.string.password_required_en);
+                mPasswordCharacters = registrationActivity.getResources().getString(R.string.password_characters_en);
+                mConfirmPassRequired = registrationActivity.getResources().getString(R.string.confirm_password_required_en);
+                mPassDoesNotMatch = registrationActivity.getResources().getString(R.string.pass_do_not_match_en);
+                break;
+
+            case AppConstant.Language.URDU:
+                binding.tvCreateYourAccount.setText(registrationActivity.getString(R.string.create_your_account_ur));
+                binding.tvFname.setText(registrationActivity.getString(R.string.fname_ur));
+                binding.tvLname.setText(registrationActivity.getString(R.string.lname_ur));
+                binding.tvEmail.setText(registrationActivity.getString(R.string.email_ur));
+                binding.tvPhoneNumber.setText(registrationActivity.getString(R.string.phone_number_ur));
+                binding.tvPassword.setText(registrationActivity.getString(R.string.password_ur));
+                binding.tvConfirmPassword.setText(registrationActivity.getString(R.string.confirm_password_ur));
+                binding.btnCreateAccount.setText(registrationActivity.getString(R.string.create_account_ur));
+
+                binding.linearLayoutName.addView(binding.linearLayoutLastName, 0);
+                binding.linearLayoutName.addView(binding.viewSpace, 1);
+                binding.linearLayoutName.addView(binding.linearLayoutFirstName, 2);
+
+                binding.etFname.setGravity(Gravity.END);
+                binding.etLname.setGravity(Gravity.END);
+                binding.etEmail.setGravity(Gravity.END);
+                binding.etPhoneNumber.setGravity(Gravity.END);
+                binding.etPassword.setGravity(Gravity.END);
+                binding.etConfirmPassword.setGravity(Gravity.END);
+
+                mFnameRequired = registrationActivity.getResources().getString(R.string.fname_required_ur);
+                mLnameRequired = registrationActivity.getResources().getString(R.string.lname_required_ur);
+                mEmailRequired = registrationActivity.getResources().getString(R.string.email_required_ur);
+                mInvalidEmail = registrationActivity.getResources().getString(R.string.invalid_email_ur);
+                mPhoneNumberRequired = registrationActivity.getResources().getString(R.string.phone_number_required_ur);
+                mPasswordRequired = registrationActivity.getResources().getString(R.string.password_required_ur);
+                mPasswordCharacters = registrationActivity.getResources().getString(R.string.password_characters_ur);
+                mConfirmPassRequired = registrationActivity.getResources().getString(R.string.confirm_password_required_ur);
+                mPassDoesNotMatch = registrationActivity.getResources().getString(R.string.pass_do_not_match_ur);
+                break;
+        }
+    }
+
+    @Override
     public void onClick(View view) {
         switch (view.getId()) {
 
 
-            case R.id.tvWithEmail:
+            case R.id.btnCreateAccount:
                 validate();
                 break;
 
@@ -56,43 +137,43 @@ public class FoodSignUpFragment extends BaseFragment implements View.OnClickList
 
 
     private void init() {
-        binding.tvWithEmail.setOnClickListener(this);
+        binding.btnCreateAccount.setOnClickListener(this);
     }
 
     private void validate() {
 
         if (binding.etFname.getText().toString().trim().equalsIgnoreCase("")) {
-            UIHelper.showToast(registrationActivity, registrationActivity.getResources().getString(R.string.fname_required_en));
+            UIHelper.showToast(registrationActivity, mFnameRequired);
             return;
         }
         if (binding.etLname.getText().toString().trim().equalsIgnoreCase("")) {
-            UIHelper.showToast(registrationActivity, registrationActivity.getResources().getString(R.string.lname_required_en));
+            UIHelper.showToast(registrationActivity, mLnameRequired);
             return;
         }
         if (binding.etEmail.getText().toString().trim().length() == 0) {
-            UIHelper.showToast(registrationActivity, registrationActivity.getResources().getString(R.string.email_required_en));
+            UIHelper.showToast(registrationActivity, mEmailRequired);
             return;
         }
         if (binding.etEmail.getText().toString().trim().length() > 0) {
             if (!CustomValidation.isValidEmail(binding.etEmail.getText().toString())) {
-                UIHelper.showToast(registrationActivity, registrationActivity.getResources().getString(R.string.invalid_email_en));
+                UIHelper.showToast(registrationActivity, mInvalidEmail);
                 return;
             }
         }
-        if (binding.etNumber.getText().toString().equals("")) {
-            UIHelper.showToast(registrationActivity, getString(R.string.phone_number_required_en));
+        if (binding.etPhoneNumber.getText().toString().equals("")) {
+            UIHelper.showToast(registrationActivity, mPhoneNumberRequired);
             return;
         }
         if (binding.etPassword.getText().toString().length() == 0) {
-            UIHelper.showToast(registrationActivity, registrationActivity.getResources().getString(R.string.password_required_en));
+            UIHelper.showToast(registrationActivity, mPasswordRequired);
             return;
         }
         if (binding.etPassword.getText().toString().length() < 6) {
-            UIHelper.showToast(registrationActivity, registrationActivity.getResources().getString(R.string.password_characters_en));
+            UIHelper.showToast(registrationActivity, mPasswordCharacters);
             return;
         }
         if (binding.etConfirmPassword.getText().toString().length() == 0) {
-            UIHelper.showToast(registrationActivity, registrationActivity.getResources().getString(R.string.confirm_password_required_en));
+            UIHelper.showToast(registrationActivity, mConfirmPassRequired);
             return;
         }
 //        if (binding.etConfirmPassword.getText().toString().length() < 6) {
@@ -100,7 +181,7 @@ public class FoodSignUpFragment extends BaseFragment implements View.OnClickList
 //            return;
 //        }
         if (!binding.etPassword.getText().toString().equals(binding.etConfirmPassword.getText().toString())) {
-            UIHelper.showToast(registrationActivity, registrationActivity.getResources().getString(R.string.pass_do_not_match_en));
+            UIHelper.showToast(registrationActivity, mPassDoesNotMatch);
             return;
         }
 
@@ -111,7 +192,7 @@ public class FoodSignUpFragment extends BaseFragment implements View.OnClickList
     public void register() {
         serviceHelper.enqueueCall(webService.userSignUp(binding.etFname.getText().toString() + binding.etLname.getText().toString(),
                 binding.etEmail.getText().toString(),
-                binding.etNumber.getText().toString(),
+                binding.etPhoneNumber.getText().toString(),
                 binding.etPassword.getText().toString(),
                 1), AppConstant.FOODPORTAL_FOOD_DETAILS.FOOD_USER_SIGNUP);
 
