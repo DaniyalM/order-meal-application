4b74292dd67f3b78bc3443c095c1680908e921de
Syed Mohsin Raza <mohsin.raza@express.com.pk>
2019-08-09 12:56:55 +0500

    OP-329 (Splash, Login & Sign up Fixes)

--- app/src/main/java/structure/com/foodportal/activity/SplashActivity.java
+++ app/src/main/java/structure/com/foodportal/activity/SplashActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ProgressBar;
 
 import jp.shts.android.storiesprogressview.StoriesProgressView;
@@ -30,9 +31,7 @@ public class SplashActivity extends AppCompatActivity implements StoriesProgress
 
 
         // storiesProgressView = findViewById(R.id.stories);
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
     @Override
